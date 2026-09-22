55eeac26d166cc8ef82fe357171f9ee790722fb1
manjit1990 <mbmanjit5@gmail.com>
2026-09-22 10:26:25 +0530

    Update version20

--- shared/src/iosMain/kotlin/com/learningapp/firesafetyservicemanagement/MainViewController.kt
+++ shared/src/iosMain/kotlin/com/learningapp/firesafetyservicemanagement/MainViewController.kt
@@ -1,5 +1,11 @@
 package com.learningapp.firesafetyservicemanagement
 
 import androidx.compose.ui.window.ComposeUIViewController
+import com.yoga.firesafety.shared.presentation.MainApp
+import com.yoga.firesafety.shared.presentation.theme.FireSafetyTheme
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController {
+    FireSafetyTheme {
+        MainApp()
+    }
+}
